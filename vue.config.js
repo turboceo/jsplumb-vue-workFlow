cc1eb98e36dacb8921b17f6ae4d0ebe8a78b9fe1
wsl-turbo <wsl@dev.com>
2024-08-16 10:35:58 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,27 @@ const path = require('path')
 const resolve = dir => {
   return path.join(__dirname, dir)
 }
+
+let mix = Object.assign
+let isDev = process.env.NODE_ENV === 'development'
+
 module.exports = {
   publicPath: './',
   chainWebpack: config => {
     config.resolve.alias.set('@', resolve('src'))
+    mix(config, {
+      externals: {
+        vue: "Vue",
+        "element-ui": "ELEMENT",
+        "vue-router": "VueRouter",
+        axios: "axios",
+        vuex: "Vuex",
+      },
+      cache: {
+        // 使用文件缓存，加速二次构建
+        type: isDev ? "memory" : "filesystem",
+      },
+    })
   },
   devServer: {
     port: 8081
